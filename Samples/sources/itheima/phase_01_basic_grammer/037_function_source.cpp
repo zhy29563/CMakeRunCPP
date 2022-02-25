4fa631b4454029a5de7ad32685963220255922b9
Steven Zhou <zhy29563@hotmail.com>
2022-02-25 12:22:50 +0800

    基础语法

---
+++ Samples/sources/itheima/phase_01_basic_grammer/037_function_source.cpp
@@ -0,0 +1,11 @@
+#include "037_function_head.h"
+
+void swap(int a, int b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+}
---
+++ Samples/sources/itheima/phase_01_basic_grammer/042_pointer_array_function.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+/**********************************************************************************************************************
+ * 指针、数组、函数
+ *  案例描述：封装一个函数，利用冒泡排序，实现对整型数组的升序排序
+ *  例如数组：int arr[10] = { 4,3,6,9,1,2,10,8,7,5 };
+ *
+ * 总结：
+ *  当数组名传入到函数作为参数时，被退化为指向首元素的指针
+ *********************************************************************************************************************/
+
+//冒泡排序函数
+void bubbleSort(int *arr, int len) // int * arr 也可以写为int arr[]
+{
+    for (int i = 0; i < len - 1; i++)
+    {
+        for (int j = 0; j < len - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+//打印数组函数
+void printArray(int arr[], int len)
+{
+    for (int i = 0; i < len; i++)
+    {
+        cout << arr[i] << endl;
+    }
+}
+
+int main()
+{
+    int arr[10] = {4, 3, 6, 9, 1, 2, 10, 8, 7, 5};
+    int len = sizeof(arr) / sizeof(int);
+
+    bubbleSort(arr, len);
+    printArray(arr, len);
+
+    system("pause");
+    return 0;
+}
